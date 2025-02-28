b734e072b40a1756ce4acbc44723cbe4bb1a238f
zeus <136583387+zeusssz@users.noreply.github.com>
2025-02-28 09:58:21 +0530

    fixed stuff

--- src/drater.go
+++ src/drater.go
@@ -1,122 +1,141 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net"
-	"os"
+    "fmt"
+    "log"
+    "net"
+    "os"
+    "path/filepath"
 
-	"github.com/pkg/sftp"
-	"golang.org/x/crypto/ssh"
+    "github.com/pkg/sftp"
+    "golang.org/x/crypto/ssh"
 )
 
 const (
-	port          = ":2022"
-	rootDir       = "./files"
-	privateKeyPath = "id_rsa" // path to priv key for auth
+    port          = ":2022"
+    rootDir       = "./files"
+    privateKeyPath = "id_rsa"
 )
 
 func auth(username, password string) bool {
-	expectedPassword := os.Getenv("SFTP_PASSWORD")
-	if expectedPassword == "" {
-		log.Fatal("SFTP_PASSWORD environment variable not set")
-	}
-	return password == expectedPassword
+    expectedPassword := os.Getenv("SFTP_PASSWORD")
+    if expectedPassword == "" {
+        log.Fatal("SFTP_PASSWORD environment variable not set")
+    }
+    return password == expectedPassword
 }
 
 func handleConnection(conn net.Conn) {
-	defer conn.Close()
-	privateBytes, err := os.ReadFile(privateKeyPath)
-	if err != nil {
-		log.Printf("Failed to read private key: %v", err)
-		return
-	}
-	privateKey, err := ssh.ParsePrivateKey(privateBytes)
-	if err != nil {
-		log.Printf("Failed to parse private key: %v", err)
-		return
-	}
+    defer conn.Close()
+    privateBytes, err := os.ReadFile(privateKeyPath)
+    if err != nil {
+        log.Printf("Failed to read private key: %v", err)
+        return
+    }
+    privateKey, err := ssh.ParsePrivateKey(privateBytes)
+    if err != nil {
+        log.Printf("Failed to parse private key: %v", err)
+        return
+    }
 
-	config := &ssh.ServerConfig{
-		PasswordCallback: func(c ssh.ConnMetadata, pass []byte) (*ssh.Permissions, error) {
-			if auth(c.User(), string(pass)) {
-				return nil, nil
-			}
-			return nil, fmt.Errorf("password rejected for %q", c.User())
-		},
-	}
+    config := &ssh.ServerConfig{
+        PasswordCallback: func(c ssh.ConnMetadata, pass []byte) (*ssh.Permissions, error) {
+            if auth(c.User(), string(pass)) {
+                return &ssh.Permissions{}, nil
+            }
+            return nil, fmt.Errorf("password rejected for %q", c.User())
+        },
+    }
 
-	config.AddHostKey(privateKey)
+    config.AddHostKey(privateKey)
 
-	_, chans, reqs, err := ssh.NewServerConn(conn, config)
-	if err != nil {
-		log.Printf("Failed to handshake: %v", err)
-		return
-	}
+    _, chans, reqs, err := ssh.NewServerConn(conn, config)
+    if err != nil {
+        log.Printf("Failed to handshake: %v", err)
+        return
+    }
 
-	go ssh.DiscardRequests(reqs)
+    go ssh.DiscardRequests(reqs)
 
-	for newChannel := range chans {
-		go handleChannel(newChannel)
-	}
+    for newChannel := range chans {
+        go handleChannel(newChannel)
+    }
 }
 
 func handleChannel(newChannel ssh.NewChannel) {
-	defer newChannel.Close()
-	channel, reqs, err := newChannel.Accept()
-	if err != nil {
-		log.Printf("Could not accept channel: %v", err)
-		return
-	}
+    if newChannel.ChannelType() != "session" {
+        newChannel.Reject(ssh.UnknownChannelType, "unknown channel type")
+        return
+    }
 
-	go ssh.DiscardRequests(reqs)
+    channel, reqs, err := newChannel.Accept()
+    if err != nil {
+        log.Printf("Could not accept channel: %v", err)
+        return
+    }
+    defer channel.Close()
 
-	sftpServer, err := sftp.NewServer(channel)
-	if err != nil {
-		log.Printf("Failed to start SFTP server: %v", err)
-		return
-	}
+    go ssh.DiscardRequests(reqs)
+    server, err := sftp.NewServer(
+        channel,
+        sftp.WithRoot(rootDir),
+    )
+    if err != nil {
+        log.Printf("Failed to start SFTP server: %v", err)
+        return
+    }
+    defer server.Close()
 
-	if err := sftpServer.Serve(); err != nil {
-		log.Printf("SFTP server error: %v", err)
-	}
+    if err := server.Serve(); err != nil {
+        log.Printf("SFTP server error: %v", err)
+    }
 }
 
 func main() {
-	if err := os.MkdirAll(rootDir, 0755); err != nil {
-		log.Fatalf("Failed to create directory: %v", err)
-	}
-	if os.Getenv("SFTP_PASSWORD") == "" {
-		log.Fatal("SFTP_PASSWORD environment variable not set")
-	}
+    absRootDir, err := filepath.Abs(rootDir)
+    if err != nil {
+        log.Fatalf("Failed to get absolute path: %v", err)
+    }
 
-	listener, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatalf("Failed to start listener: %v", err)
-	}
-	defer listener.Close()
+    if err := os.MkdirAll(absRootDir, 0755); err != nil {
+        log.Fatalf("Failed to create directory: %v", err)
+    }
 
-	addrs, err := net.InterfaceAddrs()
-	if err != nil {
-		log.Fatalf("Failed to get IP addresses: %v", err)
-	}
+    if os.Getenv("SFTP_PASSWORD") == "" {
+        log.Fatal("SFTP_PASSWORD environment variable not set")
+    }
+    if _, err := os.Stat(privateKeyPath); os.IsNotExist(err) {
+        log.Fatalf("Private key file %s does not exist. Please generate one using: ssh-keygen -t rsa -b 2048 -f id_rsa -N \"\"", privateKeyPath)
+    }
 
-	fmt.Println("SFTP Server is running on the following IP addresses:")
-	for _, addr := range addrs {
-		if ipNet, ok := addr.(*net.IPNet); ok && !ipNet.IP.IsLoopback() {
-			if ipNet.IP.To4() != nil {
-				fmt.Printf("IP: %s\n", ipNet.IP.String())
-			}
-		}
-	}
-	fmt.Printf("SFTP Server started on port%s\n", port)
+    listener, err := net.Listen("tcp", port)
+    if err != nil {
+        log.Fatalf("Failed to start listener: %v", err)
+    }
+    defer listener.Close()
 
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Printf("Failed to accept connection: %v", err)
-			continue
-		}
-		go handleConnection(conn)
-	}
+    addrs, err := net.InterfaceAddrs()
+    if err != nil {
+        log.Fatalf("Failed to get IP addresses: %v", err)
+    }
+
+    fmt.Println("SFTP Server is running on the following IP addresses:")
+    for _, addr := range addrs {
+        if ipNet, ok := addr.(*net.IPNet); ok && !ipNet.IP.IsLoopback() {
+            if ipNet.IP.To4() != nil {
+                fmt.Printf("IP: %s%s\n", ipNet.IP.String(), port)
+            }
+        }
+    }
+    fmt.Printf("SFTP Server started on port %s\n", port)
+    fmt.Printf("Files will be stored in: %s\n", absRootDir)
+
+    for {
+        conn, err := listener.Accept()
+        if err != nil {
+            log.Printf("Failed to accept connection: %v", err)
+            continue
+        }
+        go handleConnection(conn)
+    }
 }
